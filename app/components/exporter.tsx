9b0510e21919a3aa30b8a778595c3e2d8e21c3a0
Xiao Qingyu <ixiaoqy@gmail.com>
2024-07-24 04:37:37 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -541,7 +541,7 @@ export function ImagePreviewer(props: {
           <div>
             <div className={styles["main-title"]}>白羽 AI 聊天</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              aichat.xiaoqy.com
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
